5978717e5e0c9fc7eab8709af05ce78bb8552e42
durianbeetroot <durianbeetroot@gmail.com>
2026-05-03 10:29:26 +0700

    added yourcat

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Header from "@/app/components/header";
 import Footer from "@/app/components/footer";
 import CatBreeds from "@/app/components/catbreeds";
 import MoreFromUs from "@/app/components/morefromus";
+import YourCat from "./components/yourcat";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <CatBreeds />
+      <YourCat />
       <MoreFromUs />
       <Footer />
       
